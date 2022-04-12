717e800c59ab72cc553d38775497ff215824a317
Rajasekhar524 <rajshekhar.k321@gmail.com>
2022-04-12 15:38:36 +0530

    second

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Header from './components/Header/Header';
+import './default.scss';
+
+function App() {
+  return (
+    <div className="App">
+     <Header />
+    </div>
+  );
+}
+export default App;
